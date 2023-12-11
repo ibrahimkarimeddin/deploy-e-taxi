53a3ad62ec5253090d0440e6abafe2b37dc150f1
ibrahimkarimeddin <ibrahimkarimeddin@gmail.com>
2023-12-12 01:07:11 +0300

    ...

--- src/Layout/Dashboard/LyTable.tsx
+++ src/Layout/Dashboard/LyTable.tsx
@@ -15,6 +15,7 @@ const LyTable = (props?: any) => {
             columns={props?.column}
             data={props?.data}
             progressPending={props?.isLoading}
+  
             noDataComponent={<h6 className="my-4">{t("no_records")}</h6>}
             noHeader
             pagination
